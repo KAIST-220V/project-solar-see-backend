51871b4e404e71f0f4570190adde1cad4ca76a24
skykhs3 <skykhs3@naver.com>
2024-11-22 00:00:34 +0900

    change endpoint name

--- panel/urls.py
+++ panel/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/v1/map/panels', CoordinatesView.as_view()),
+    path('api/v1/map/image', CoordinatesView.as_view()),
     path('api/v1/game/image', GameImageView.as_view()),
     path('api/v1/game/score', GameScoreView.as_view()),
     path('api/v1/game/ranking', GameRankingView.as_view()),
