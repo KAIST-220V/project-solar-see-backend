272cf898922db1024d00efed3d3617b2a5ad70fc
skykhs3 <skykhs3@naver.com>
2024-11-24 06:10:00 +0000

    fix: serializers error

--- panel/serializers.py
+++ panel/serializers.py
@@ -7,9 +7,9 @@ class MapPanelFullSerializer(serializers.ModelSerializer):
     class Meta:
         model = MapImage
         fields = ['id', 'image_url', 'latitude', 'longitude', 'area_m2', 
-                 'created_at', 'updated_at', 'deleted_at']
+                  'created_at', 'updated_at', 'deleted_at']
 
     def get_image_url(self, obj):
         request = self.context.get('request')
-        image_url = obj.image.url
+        image_url = obj.image_url.url
         return request.build_absolute_uri(image_url) if request else image_url
